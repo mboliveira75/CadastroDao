d939406393515a17f02271a92cdd7e6ffcc2c0df
Marcelo Oliveira <mboliveira75@gmail.com>
2012-07-04 22:30:49 -0300

    Criacao DAO

---
+++ src/com/xpto/exception/PessoaException.java
@@ -0,0 +1,15 @@
+package com.xpto.exception;
+
+public class PessoaException extends RuntimeException {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public PessoaException(String mensagem){
+		
+		super(mensagem);
+	}
+
+}
